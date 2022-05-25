1701c26a7d807deca7b3c1e7dfa82d6f1f23b771
Jorge Zetien <jzetien@lsv-tech.com>
2022-05-25 18:11:16 -0500

    actividad realizada en clase 25-05-2022 se agregaron 3 botones y se reestructuro el codigo de acuerdo a lo visto en clases

--- lib/view/count_screen.dart
+++ lib/view/count_screen.dart
@@ -10,43 +10,83 @@ class Contador2 extends StatefulWidget {
 class _Contador2State extends State<Contador2> {
   int count = 10;
 
+  void incrementarValor() {
+    setState(() {
+      count++;
+    });
+  }
+
+  void decrementarValor() {
+    setState(() {
+      count--;
+    });
+  }
+
+  void reiniciarValor() {
+    setState(() {
+      count = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     const sizeTextColorA = TextStyle(fontSize: 24, color: Colors.white);
     const sizeTextColorB = TextStyle(fontSize: 24, color: Colors.red);
 
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 3, 3, 3),
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 242, 75, 4),
-        title: const Text("Contador"),
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text("Number push", style: sizeTextColorA),
-          Text('$count', style: sizeTextColorB),
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red,
-        child: const Icon(Icons.access_alarms),
-        onPressed: () {
-          showMessage();
-          contadorIncremeta();
-        },
-      ),
+        backgroundColor: const Color.fromARGB(255, 3, 3, 3),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 242, 75, 4),
+          title: const Text("Contador"),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text("Number push", style: sizeTextColorA),
+            Text('$count', style: sizeTextColorB),
+          ],
+        )),
+        
+        floatingActionButton: FloatingCustomButonGroup(
+            increase: incrementarValor,
+            decrease: decrementarValor,
+            reset: reiniciarValor));
+  }
+}
+
+class FloatingCustomButonGroup extends StatelessWidget {
+  final Function increase;
+  final Function decrease;
+  final Function reset;
+
+  const FloatingCustomButonGroup({
+    Key? key,
+    required this.increase,
+    required this.decrease,
+    required this.reset,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        FloatingActionButton(
+          backgroundColor: Colors.red,
+          child: const Icon(Icons.exposure_plus_1),
+          onPressed: () => increase(),
+        ),
+        FloatingActionButton(
+          backgroundColor: Colors.red,
+          child: const Icon(Icons.exposure_minus_1),
+          onPressed: () => decrease(),
+        ),
+        FloatingActionButton(
+          backgroundColor: Colors.red,
+          child: const Icon(Icons.refresh),
+          onPressed: () => reset(),
+        ),
+      ],
     );
-  }
-
-  void showMessage() {
-    print('Hi, Again');
-  }
-
-  void contadorIncremeta() {
-    setState(() {
-      count++;
-    });
   }
 }
